7dfcd65987bdac790246c3e74f1033e6fa0c6498
Hady Walied <hadywaliedkamel@gmail.com>
2024-04-01 00:33:00 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
+def handle(connection):
+    with connection:
+        while True:
+            data = connection.recv(1024)
+            if data:
+                connection.sendall(b"+PONG\r\n")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,11 +16,11 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ =server_socket.accept() # wait for client
+    
     while True:
-        data = connection.recv(1024)
-        if data:
-            connection.sendall(b"+PONG\r\n")
+        connection, _ =server_socket.accept() # wait for client
+        _thread = threading.Thread(target= handle, args=[connection])
+        _thread.start()
 
 
 if __name__ == "__main__":
