2881e62c7c788f07dd40e8450a0913cdc62913a8
Hady Walied <hadywaliedkamel@gmail.com>
2024-04-01 03:44:44 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 # Uncomment this to pass the first stage
 import socket
 import threading
+import re
+
+class Regexes :
+    PING_REGEX = re.compile(r'ping\b', re.IGNORECASE)
+    ECHO_REGEX = re.compile(r'echo\b(.*)', re.IGNORECASE)
+    
 
 def handle(connection):
     with connection:
         while True:
-            data = connection.recv(1024)
-            if data:
+            data = connection.recv(1024).decode()
+            if re.search(Regexes.PING_REGEX, data):
                 connection.sendall(b"+PONG\r\n")
+            elif re.search(Regexes.ECHO_REGEX, data):
+                resp = data.split("\r\n")[-2]
+                resp = f"${len(resp)}\r\n{resp}\r\n"
+                resp = resp.encode()
+                connection.sendall(resp)
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
